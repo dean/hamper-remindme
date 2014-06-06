09e47feb6cc9cfc8ed16898c95ae0c51512cb6f6
Chance Zibolski <chance.zibolski@gmail.com>
2014-06-05 22:52:50 -0700

    Version 0.2
    Get rid of hyphen in package name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
 with open('requirements.txt') as f:
     requirements = [l.strip() for l in f]
 
 setup(
-    name='hamper-remindme',
-    version='0.1',
-    packages=['hamper-remindme'],
+    name='hamper_remindme',
+    version='0.2',
+    packages=['hamper_remindme'],
     author='Dean Johnson',
     author_email='deanjohnson222@gmail.com',
     url='https://github.com/dean/hamper-remindme',
